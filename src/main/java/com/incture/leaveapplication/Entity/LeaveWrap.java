97cdb602e46b9f3eee216f08d42f922cf741a33d
yash-prabhakar-singh <100512635+yash-prabhakar-singh@users.noreply.github.com>
2022-03-11 22:45:36 +0530

    Update LeaveWrap.java

--- src/main/java/com/incture/leaveapplication/Entity/LeaveWrap.java
+++ src/main/java/com/incture/leaveapplication/Entity/LeaveWrap.java
@@ -1,1 +1,63 @@
+package com.incture.leaveapplication.Entity;
 
+
+
+
+public class LeaveWrap {
+    
+    
+private int level;
+private char type;
+private String description;
+private String appliedby;
+
+
+
+
+    public LeaveWrap() {
+
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+   
+
+    public LeaveWrap(int level, char type, String description, String appliedby) {
+        this.level = level;
+        this.type = type;
+        this.description = description;
+        
+        this.appliedby= appliedby;
+    }
+
+   
+    public String getAppliedby() {
+        return appliedby;
+    }
+
+    public void setAppliedby(String appliedby) {
+        this.appliedby = appliedby;
+    }
+}
